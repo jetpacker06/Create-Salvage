7735cd2cc0a73dd6fa4d5b291d6163924d72dbd5
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-07-30 15:37:36 -0500

    Initial commit

--- src/main/java/com/jetpacker06/createsalvage/CreateSalvage.java
+++ src/main/java/com/jetpacker06/createsalvage/CreateSalvage.java
@@ -1,4 +1,4 @@
-package com.jetpacker06.modpackage;
+package com.jetpacker06.createsalvage;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -6,22 +6,23 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-//Mod ID can have ONLY LOWERCASE LETTERS. No numbers, no underscores, no characters, no capitals, no spaces.
-@Mod("yourmodidhere")
-public class ModMainClass {
-    public static final String MOD_ID = "yourmodidhere";
-    public static final Logger LOGGER = LogManager.getLogger();
 
-    public ModMainClass() {
+@Mod("createsalvage")
+public class CreateSalvage {
+    public static final String MOD_ID = "createsalvage";
+    public static void log(Object o) {
+        LogManager.getLogger().info(o);
+    }
+
+    public CreateSalvage() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModBlocks.register(eventBus);
         ModItems.register(eventBus);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
+    private void commonSetup(final FMLCommonSetupEvent event)
     {
 
     }
